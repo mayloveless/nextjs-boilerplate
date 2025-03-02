0ee8120c1e3fc7ffca328e60f2e653c2ed266fc6
wlf <wanglingfei@yqkj.com>
2025-03-02 19:22:24 +0800

    fix: ts

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,11 +1,12 @@
-// @ts-ignore
-import { NextResponse } from 'next/server'
- 
-export async function GET() {
-  return NextResponse.json({ message: 'Hello World!' })
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// 定义 GET 请求处理函数
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  return res.status(200).json({ message: 'Hello World!' })
 }
 
-export async function POST(request: Request) {
-  const data = await request.json()
-  return NextResponse.json({ message: 'Hello World!', data })
+// 定义 POST 请求处理函数
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const data = await req.body
+  return res.status(200).json({ message: 'Hello World!', data })
 }
